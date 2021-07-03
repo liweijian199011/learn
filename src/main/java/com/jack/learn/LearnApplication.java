d0b0a423a159b9393f13235f7dc338bbd27834df
liweijian <liweijian@vipkid.com.cn>
2021-07-03 21:38:25 +0800

    添加dubbo配置

--- src/main/java/com/jack/learn/LearnApplication.java
+++ src/main/java/com/jack/learn/LearnApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.jack.learn.mapper")
+@MapperScan(basePackages = {"com.jack.learn.mapper", "com.jack.learn.dao.mapper"})
 @SpringBootApplication
 public class LearnApplication {
 
